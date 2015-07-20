22614cb177d47296e468ac5b7139afd887350aed
Conrad Stansbury <chstansbury@gmail.com>
2015-07-19 22:12:15 -0700

    Minimal running project. First step of ReProCS running.

--- reprocs/src/main/scala/reprocs/ReProCS.scala
+++ reprocs/src/main/scala/reprocs/ReProCS.scala
@@ -1,5 +1,39 @@
 package reprocs
 
-class ReProCS {
+import breeze.linalg._
 
+object ReProCSUtils {
+  def approxBasisEnergy (m: DenseMatrix[Double], p: Double):
+      (DenseMatrix[Double], DenseVector[Double]) = {
+    val svd.SVD(u,s,v) = svd(m)
+    val t = sum(s :* s)
+    var q = 0.0
+    List.range(0,s.length).foreach(i => {
+      q += s(i)*s(i)
+      if (q > p*t) {
+        return (u(::, 0 to i), s(0 to i));
+      }
+    })
+    return (u, s);
+  }
+
+  def approxBasisN (m: DenseMatrix[Double], n: Int):
+      (DenseMatrix[Double], DenseVector[Double]) = {
+    val svd.SVD(u,s,v) = svd(m)
+    return (u(::, 0 to n - 1), s(0 to n - 1))
+  }
+}
+
+object ReProCS extends App {
+  def runReProCS(m: DenseMatrix[Double], tTrain: Int, q: Double, b: Double,
+    alpha: Double = 20, kMin: Double = 3, kMax: Double = 10):
+      (DenseMatrix[Double], DenseMatrix[Double], DenseMatrix[Double]) = {
+    val (p, sigma) = ReProCSUtils.approxBasisEnergy(m(::, 0 to tTrain - 1), b)
+    // lots more here
+
+    return (m, m, m)
+  }
+
+  val a = DenseMatrix.rand(3,3)
+  println(runReProCS(a, 2, 0, 0.9))
 }

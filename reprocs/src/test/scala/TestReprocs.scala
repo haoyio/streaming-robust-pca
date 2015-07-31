f445f662a776f1268bde72948a52c9349c523373
haoyio <haoyi@stanford.edu>
2015-07-30 18:35:11 -0700

    tried weighted l1-min w/o constraints, solver inaccurate (see comments)

--- reprocs/src/test/scala/TestReprocs.scala
+++ reprocs/src/test/scala/TestReprocs.scala
@@ -6,6 +6,7 @@ import reprocs._
 object TestReprocs extends App {
   val reprocs = Reprocs(DenseMatrix.rand[Double](10, 10))
   testL1Min()
+  testWeightedL1Min()
 
   def testL1Min(): Unit = {
     val f = new DiffFunction[DenseVector[Double]] {
@@ -17,6 +18,24 @@ object TestReprocs extends App {
     val optimizer = new ProjectedQuasiNewton(tolerance = Optim.Tolerance)
     val res = optimizer.minimize(f, init)
     assert(f(res) < Optim.Tolerance)
-    println("minimizer = " + res + " minimum = " + f(res))
+    println("minimizer = " + res + "\nminimum = " + f(res))
+  }
+
+  // TODO: solver inaccurate, figure out how to use box constraints with ProjectedQuasiNewton
+  def testWeightedL1Min(): Unit = {
+    val f = new DiffFunction[DenseVector[Double]] {
+      def calculate(x: DenseVector[Double]): (Double, DenseVector[Double]) = {
+        val lambda = 0.1d
+        val obj = norm(x(0 until 10), 1d) + lambda * norm(x(10 until 20), 1d)
+        val grad = DenseVector.vertcat(Optim.lossL1Grad(x(0 until 10)), lambda * Optim.lossL1Grad(x(10 until 20)))
+        (obj, grad)
+      }
+    }
+
+    val init = DenseVector.rand[Double](20)
+    val optimizer = new ProjectedQuasiNewton(tolerance = Optim.Tolerance)
+    val res = optimizer.minimize(f, init)
+    // assert(f(res) < Optim.Tolerance)
+    println("minimizer = " + res + "\nminimum = " + f(res))
   }
 }
